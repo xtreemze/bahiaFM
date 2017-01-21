57408137a3b2cc8025fee861507b54fe9340c643
Carlos Velasco <carlos@2activedesign.com>
2017-01-21 20:19:39 +0100

    Lint Updates

---
+++ .eslintrc.js
@@ -0,0 +1,42 @@
+// !minifyOnSave
+// minifyOnSave: false
+// !minOnSave
+// minOnSave: false
+module.exports = {
+  "extends": "airbnb",
+  "installedESLint": true,
+  // "plugins": ["react"],
+  "ecmaVersion": 5,
+  "ecmaFeatures": {
+    "jsx": false,
+    "modules": false,
+    "arrowFunctions": false,
+    "classes": true,
+    "spread": true,
+  },
+  "env": {
+    "browser": true,
+    "jquery": true,
+		"worker": true,
+		"serviceworker": true,
+  },
+  "rules": {
+    // "indent": [ "error", "tab" ],
+    // "linebreak-style": [ "error", "windows" ],
+    // "quotes": [ "error", "single" ],
+    // "semi": [ "error", "always" ]
+    // "newline-per-chained-call": ["off"],
+    "no-unused-vars": ["off"],
+    "max-len": ["off", 100, 4],
+    "no-useless-concat": ["off"],
+    "no-var": ["off"],
+    // "prefer-arrow-callback": ["off"],
+    "no-undef": ["off"],
+    "prefer-template": ["off"],
+    "prefer-arrow-callback": ["off"]
+  },
+  "globals": {
+    "Materialize": true,
+    "intervalID": true
+  },
+};
