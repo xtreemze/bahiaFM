fa6e6fc11dacea4b7618c46703ac40629c9cf419
Carlos Velasco <carlos@2activedesign.com>
2017-08-08 17:32:10 +0200

    Prettify

--- js/playPause.js
+++ js/playPause.js
@@ -1,21 +1,21 @@
 window.offlineTrip = false;
-window.radioString = 'http://192.30.164.78:8000/bahia';
+window.radioString = "http://192.30.164.78:8000/bahia";
 // window.radioString = 'http://192.30.164.78:8000/bahiaCabina';
 // window.radioString = 'http://192.30.164.78:8000/stream';
-window.audioElement = document.getElementById('audioE');
-window.button = document.getElementById('button1');
+window.audioElement = document.getElementById("audioE");
+window.button = document.getElementById("button1");
 window.audioElement.src = window.radioString;
-window.audioElement.crossOrigin = 'anonymous';
+window.audioElement.crossOrigin = "anonymous";
 window.buttonPlay = function addPlaying() {
-  window.button.className = ('playing');
+  window.button.className = "playing";
   window.offlineTrip = false;
 };
 window.buttonPause = function addPlaying() {
-  window.button.className = ('paused');
+  window.button.className = "paused";
   window.offlineTrip = false;
 };
 window.buttonError = function addError() {
-  window.button.className = ('error');
+  window.button.className = "error";
   window.offlineTrip = true;
 };
 window.checkPlay = function check() {
@@ -28,30 +28,35 @@ window.checkPlay = function check() {
   }
   if (!window.navigator.onLine && window.audioElement.paused) {
     window.buttonError();
-  } else if (window.offlineTrip && window.navigator.onLine && window.audioElement
-    .paused) {
+  } else if (
+    window.offlineTrip &&
+    window.navigator.onLine &&
+    window.audioElement.paused
+  ) {
     window.audioElement.src = window.radioString;
     window.audioElement.play();
     window.offlineTrip = false;
   }
 };
 window.scaleDown = function smallButton() {
-  window.button.classList.add('scaleDown');
+  window.button.classList.add("scaleDown");
   if (window.navigator.vibrate) window.navigator.vibrate(10);
 };
 window.scaleNormal = function regularButton() {
-  window.button.classList.remove('scaleDown');
+  window.button.classList.remove("scaleDown");
   if (window.navigator.vibrate) window.navigator.vibrate(10);
 };
-window.button.addEventListener('click', window.checkPlay, false);
-window.button.addEventListener('mousedown', window.scaleDown, false);
-window.button.addEventListener('touchstart', window.scaleDown, { passive: true });
-window.button.addEventListener('mouseup', window.scaleNormal, false);
-window.button.addEventListener('touchend', window.scaleNormal, false);
-window.audioElement.addEventListener('stalled', window.buttonError, false);
-window.audioElement.addEventListener('paused', window.buttonPlay, false);
-window.audioElement.addEventListener('error', window.buttonError, false);
-window.audioElement.addEventListener('abort', window.buttonError, false);
-window.audioElement.addEventListener('playing', window.buttonPause, false);
-window.button.className = 'error';
+window.button.addEventListener("click", window.checkPlay, false);
+window.button.addEventListener("mousedown", window.scaleDown, false);
+window.button.addEventListener("touchstart", window.scaleDown, {
+  passive: true
+});
+window.button.addEventListener("mouseup", window.scaleNormal, false);
+window.button.addEventListener("touchend", window.scaleNormal, false);
+window.audioElement.addEventListener("stalled", window.buttonError, false);
+window.audioElement.addEventListener("paused", window.buttonPlay, false);
+window.audioElement.addEventListener("error", window.buttonError, false);
+window.audioElement.addEventListener("abort", window.buttonError, false);
+window.audioElement.addEventListener("playing", window.buttonPause, false);
+window.button.className = "error";
 window.audioElement.play();
